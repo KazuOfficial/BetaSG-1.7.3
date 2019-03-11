c1c9228f6858220284c7f3333eaa973d760002b8
KazuGod <kazuzmafii@gmail.com>
2019-03-11 23:08:14 +0100

    Improving teleporting...

--- src/me/kazu/betasg/listeners/BlockListeners.java
+++ src/me/kazu/betasg/listeners/BlockListeners.java
@@ -12,7 +12,7 @@ public class BlockListeners extends BlockListener {
 		if (e.getLine(0).equalsIgnoreCase("[BetaSG]"))
 		{
 			e.setLine(0,"§a[BetaSG]");
-			e.setLine(1, "§2" + PlayerListeners.players + "§2/12");
+			e.setLine(1, "§2" + PlayerListeners.size + "§2/12");
 			e.setLine(2, "§aMap: " + "§2" + map);
 			e.setLine(3, "§aClick to join!!");
 		}
--- src/me/kazu/betasg/listeners/PlayerListeners.java
+++ src/me/kazu/betasg/listeners/PlayerListeners.java
@@ -1,10 +1,11 @@
 package me.kazu.betasg.listeners;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
@@ -14,11 +15,8 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerListeners extends PlayerListener{
 	
-	ArrayList<String> arenaplayers = new ArrayList<String>();
-	
-	public static int players = 0;
-	
-	boolean isOnArena = false;
+	static ArrayList<String> arenaplayers = new ArrayList<String>();
+	static int size = arenaplayers.size();
 	
 	public void onPlayerInteract(PlayerInteractEvent e)
 	{
@@ -31,72 +29,78 @@ public class PlayerListeners extends PlayerListener{
 			if (s.getLine(0).equalsIgnoreCase("§a[BetaSG]"))
 			{
 				Player p = e.getPlayer();
-				Player target = Bukkit.getServer().getPlayer(args[0]);
+				World mfworld = Bukkit.getServer().getWorld("MFWorld");
 				
+				Location loc1 = new Location(mfworld, 162, 45, 1098);
+				Location loc2 = new Location(mfworld, 157, 47, 1138);
+				Location loc3 = new Location(mfworld, 160, 39, 1180);
+				Location loc4 = new Location(mfworld, 162, 45, 1098);
+				Location loc5 = new Location(mfworld, 157, 47, 1138);
+				Location loc6 = new Location(mfworld, 160, 39, 1180);
+				Location loc7 = new Location(mfworld, 162, 45, 1098);
+				Location loc8 = new Location(mfworld, 157, 47, 1138);
+				Location loc9 = new Location(mfworld, 160, 39, 1180);
+				Location loc10 = new Location(mfworld, 162, 45, 1098);
+				Location loc11 = new Location(mfworld, 157, 47, 1138);
+				Location loc12 = new Location(mfworld, 160, 39, 1180);
 				
-				Location spongebob = new Location(Bukkit.getServer().getWorld("MFWorld"), 162, 45, 1098);
-				Location squidward = new Location(Bukkit.getServer().getWorld("MFWorld"), 157, 47, 1138);
-				Location patrick = new Location(Bukkit.getServer().getWorld("MFWorld"), 160, 39, 1180);
+				if(size == 0)
+				{
+					p.teleport(loc1);
+				}
+				if(size == 1)
+				{
+					p.teleport(loc2);
+				}
+				if(size == 2)
+				{
+					p.teleport(loc3);
+				}
+				if(size == 3)
+				{
+					p.teleport(loc4);
+				}
+				if(size == 4)
+				{
+					p.teleport(loc5);
+				}
+				if(size == 5)
+				{
+					p.teleport(loc6);
+				}
+				if(size == 6)
+				{
+					p.teleport(loc7);
+				}
+				if(size == 7)
+				{
+					p.teleport(loc8);
+				}
+				if(size == 8)
+				{
+					p.teleport(loc9);
+				}
+				if(size == 9)
+				{
+					p.teleport(loc10);
+				}
+				if(size == 10)
+				{
+					p.teleport(loc11);
+				}
+				if(size == 11)
+				{
+					p.teleport(loc12);
+				}
+				else if(size > 11)
+				{
+					p.sendMessage(ChatColor.DARK_RED + "Map is full!");
+					return;
+				}
 				
-				if(players == 0)
-				{
-					p.teleport(spongebob);
-				}
-				else if(players == 1)
-				{
-					p.teleport(squidward);
-				}
-				else if(players == 2)
-				{
-					p.teleport(patrick);
-				}
-				else if(players == 3)
-				{
-					p.teleport(squidward);
-				}
-				else if(players == 4)
-				{
-					p.teleport(patrick);
-				}
-				else if(players == 5)
-				{
-					p.teleport(squidward);
-				}
-				else if(players == 6)
-				{
-					p.teleport(patrick);
-				}
-				else if(players == 7)
-				{
-					p.teleport(squidward);
-				}
-				else if(players == 8)
-				{
-					p.teleport(patrick);
-				}
-				else if(players == 9)
-				{
-					p.teleport(squidward);
-				}
-				else if(players == 10)
-				{
-					p.teleport(patrick);
-				}
-				else if(players == 11)
-				{
-					p.teleport(squidward);
-				}
-				else if(players == 12)
-				{
-					p.teleport(patrick);
-				}
-				else return;
-				
-				players++;
-				String str = Integer.toString(players);
+				arenaplayers.add(p.getName());
+				String str = Integer.toString(size);
 				s.setLine(1, str);
-				
-				arenaplayers.add(target.getName());
 			}
 		}
 	}

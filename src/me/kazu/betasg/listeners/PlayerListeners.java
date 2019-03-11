cc8072538e8a3e0e82a608bd3edc0ea2f437a112
KazuGod <kazuzmafii@gmail.com>
2019-03-11 21:52:24 +0100

    Added freezing after joining to the arena.

--- src/me/kazu/betasg/listeners/PlayerListeners.java
+++ src/me/kazu/betasg/listeners/PlayerListeners.java
@@ -1,6 +1,7 @@
 package me.kazu.betasg.listeners;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -13,7 +14,8 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerListeners extends PlayerListener{
 	
-	ArrayList<String> frozen = new ArrayList<String>();
+	ArrayList<String> arenaplayers = new ArrayList<String>();
+	
 	public static int players = 0;
 	
 	boolean isOnArena = false;
@@ -28,28 +30,91 @@ public class PlayerListeners extends PlayerListener{
 			
 			if (s.getLine(0).equalsIgnoreCase("§a[BetaSG]"))
 			{
-					Player p = e.getPlayer();
-					Location lc = new Location(Bukkit.getServer().getWorld("frakcje"), 6, 6, 6);
-					p.teleport(lc);
-					players++;
-					String str = Integer.toString(players);
-					s.setLine(1, str);
-					isOnArena = true;
+				Player p = e.getPlayer();
+				Player target = Bukkit.getServer().getPlayer(args[0]);
+				
+				
+				Location spongebob = new Location(Bukkit.getServer().getWorld("MFWorld"), 162, 45, 1098);
+				Location squidward = new Location(Bukkit.getServer().getWorld("MFWorld"), 157, 47, 1138);
+				Location patrick = new Location(Bukkit.getServer().getWorld("MFWorld"), 160, 39, 1180);
+				
+				if(players == 0)
+				{
+					p.teleport(spongebob);
+				}
+				else if(players == 1)
+				{
+					p.teleport(squidward);
+				}
+				else if(players == 2)
+				{
+					p.teleport(patrick);
+				}
+				else if(players == 3)
+				{
+					p.teleport(squidward);
+				}
+				else if(players == 4)
+				{
+					p.teleport(patrick);
+				}
+				else if(players == 5)
+				{
+					p.teleport(squidward);
+				}
+				else if(players == 6)
+				{
+					p.teleport(patrick);
+				}
+				else if(players == 7)
+				{
+					p.teleport(squidward);
+				}
+				else if(players == 8)
+				{
+					p.teleport(patrick);
+				}
+				else if(players == 9)
+				{
+					p.teleport(squidward);
+				}
+				else if(players == 10)
+				{
+					p.teleport(patrick);
+				}
+				else if(players == 11)
+				{
+					p.teleport(squidward);
+				}
+				else if(players == 12)
+				{
+					p.teleport(patrick);
+				}
+				else return;
+				
+				players++;
+				String str = Integer.toString(players);
+				s.setLine(1, str);
+				
+				arenaplayers.add(target.getName());
 			}
 		}
 	}
 	
 	@Override
     public void onPlayerMove(PlayerMoveEvent e){
-		if(isOnArena == true)
+		Player p = e.getPlayer();
+		if(arenaplayers.contains(p.getName()))
 		{
-	        Player p = e.getPlayer();
-	        Location location = p.getLocation();
-	        p.teleport(location);
-		}
-		else if(isOnArena == false)
-		{
-			return;
+			e.setTo(e.getFrom());
+			try
+			{
+			    Thread.sleep(10000);
+			}
+			catch(InterruptedException ex)
+			{
+			    Thread.currentThread().interrupt();
+			}
 		}
     }
 
